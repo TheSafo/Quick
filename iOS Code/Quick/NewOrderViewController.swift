6632cc25337c29d4e2234aa57b32dc133282b1b8
Jake Saferstein <jakesafo@gmail.com>
2017-02-04 17:18:24 -0500

    comit for david

--- iOS Code/Quick/NewOrderViewController.swift
+++ iOS Code/Quick/NewOrderViewController.swift
@@ -11,29 +11,19 @@ import UIKit
 
 class NewOrderViewController: UIViewController {
     
-//    var description: String?
-//    var price: Double?
+    let descriptionInput = UITextView()
+    let priceInput = UITextField()
+    let pickupNameInput = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //Config
+        descriptionInput.isEditable = true
+        
+        //Add
+        
+        //Constrain
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

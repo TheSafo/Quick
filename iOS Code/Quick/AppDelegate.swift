ac8edf125023e5e5cce441da985870871b680c67
Jake Saferstein <jakesafo@gmail.com>
2017-02-04 17:01:19 -0500

    Lit test is working bbbbbbb

--- iOS Code/Quick/AppDelegate.swift
+++ iOS Code/Quick/AppDelegate.swift
@@ -44,6 +44,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         CurrentLocation.sharedInstance.startLocationManager()
         
+        ServerAPI.sharedInstance.testPutRequestToMarko()
+        
         return true
     }
 
--- iOS Code/Quick/ServerAPI.swift
+++ iOS Code/Quick/ServerAPI.swift
@@ -8,10 +8,24 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class ServerAPI: NSObject {
     static let sharedInstance = ServerAPI()
     
     
-    
+    func testPutRequestToMarko() {
+        
+        let params = ["name":"marlena \"the GOAT\" fejzo",
+                      "id":UUID().uuidString]
+        
+        Alamofire.request("http://10.38.44.7:42069/profile", method: .post, parameters: params, encoding: JSONEncoding.default, headers: [:])
+            .validate(statusCode: 200..<300)
+            .responseJSON { response in
+                print(response.request as Any)  // original URL request
+                print(response.response as Any) // URL response
+                print(response.result.value as Any)   // result of response serialization
+        }
+        
+    }
 }

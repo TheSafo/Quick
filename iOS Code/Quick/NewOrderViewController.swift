9d3b61982b7530f42dc9904ab60cf6e7695d5635
David Branse <David@davids-macbook-pro-2.local>
2017-02-04 13:08:28 -0500

    New Order view controller added

---
+++ iOS Code/Quick/NewOrderViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NewOrderViewController.swift
+//  Quick
+//
+//  Created by David Branse on 2/4/17.
+//  Copyright © 2017 Jake Saferstein. All rights reserved.
+//
+
+import UIKit
+
+
+class NewOrderViewController: UIViewController {
+    
+    var description: String?
+    var price: Double?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

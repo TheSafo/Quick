804fca8e0183db277095a48a1ee48df3300c0757
Jake Saferstein <jakesafo@gmail.com>
2017-02-04 11:30:40 -0500

    Initial Commit

---
+++ Quick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quick
+//
+//  Created by Jake Saferstein on 2/4/17.
+//  Copyright © 2017 Jake Saferstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

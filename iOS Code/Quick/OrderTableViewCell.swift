6b96743c0ce71db0fde1868419e38919d94e2d62
Jake Saferstein <jakesafo@gmail.com>
2017-02-04 13:07:09 -0500

    Some more setup

---
+++ iOS Code/Quick/OrderTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  OrderTableViewCell.swift
+//  Quick
+//
+//  Created by Jake Saferstein on 2/4/17.
+//  Copyright © 2017 Jake Saferstein. All rights reserved.
+//
+
+import UIKit
+
+struct OrderData {
+    
+    var description: String
+    var person: String
+    var destName: String
+    
+    func getDistance() -> Double {
+        return 0.7
+    }
+}
+
+class OrderTableViewCell: UITableViewCell {
+    
+    let distanceLbl = UILabel()
+    let senderLbl = UILabel()
+    let placeLbl = UILabel()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setOrderData(_ data: OrderData) {
+        
+        
+    }
+
+}

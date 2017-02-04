d2f8866878aa89af4c1c350e6604a830e55ab9a3
David Branse <David@davids-macbook-pro-2.local>
2017-02-04 15:12:42 -0500

    added serverAPI class

---
+++ iOS Code/Quick/ServerAPI.swift
@@ -0,0 +1,17 @@
+//
+//  ServerAPI.swift
+//  Quick
+//
+//  Created by David Branse on 2/4/17.
+//  Copyright © 2017 Jake Saferstein. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ServerAPI: NSObject {
+    static let sharedInstance = ServerAPI()
+    
+    
+    
+}

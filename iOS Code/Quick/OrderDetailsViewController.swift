5fbdc3de535a5a694d815cc76709f5e865dc29a6
David Branse <David@davids-macbook-pro-2.local>
2017-02-04 21:52:08 -0500

    random stuff in various files

---
+++ iOS Code/Quick/OrderDetailsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  OrderDetailsViewController.swift
+//  Quick
+//
+//  Created by David Branse on 2/4/17.
+//  Copyright © 2017 Jake Saferstein. All rights reserved.
+//
+
+import UIKit
+
+class OrderDetailsViewController: UIViewController {
+
+    var order: OrderData
+    
+    init(order: OrderData) {
+        self.order = order
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
